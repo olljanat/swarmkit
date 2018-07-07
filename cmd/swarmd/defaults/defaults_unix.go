bcce359072f3043aa1cd0373334e9d463f41832b
Olli Janatuinen <olli.janatuinen@gmail.com>
2018-07-07 22:06:48 +0300

    a

---
+++ cmd/swarmd/defaults/defaults_unix.go
@@ -0,0 +1,9 @@
+// +build !windows
+
+package defaults
+
+// ControlAPISocket is the default path where clients can contact the swarmd control API.
+var ControlAPISocket = "/var/run/swarmd.sock"
+
+// StateDir is the default path to the swarmd state directory
+var StateDir = "/var/lib/swarmd"
--- cmd/swarmd/defaults/defaults_windows.go
+++ cmd/swarmd/defaults/defaults_windows.go
@@ -1,7 +1,9 @@
+// +build windows
+
 package defaults
 
 // ControlAPISocket is the default path where clients can contact the swarmd control API.
-var ControlAPISocket = "/var/run/swarmd.sock"
+var ControlAPISocket = "//./pipe/swarmd"
 
 // StateDir is the default path to the swarmd state directory
-var StateDir = "/var/lib/swarmd"
+var StateDir = `C:\ProgramData\swarmd`
