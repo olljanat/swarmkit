2ac022d95d1c4fa026d88f7e3c64e80939daffd9
Olli Janatuinen <olli.janatuinen@gmail.com>
2018-07-08 00:24:44 +0300

    jsdas

--- cmd/swarmd/defaults/defaults_windows.go
+++ cmd/swarmd/defaults/defaults_windows.go
@@ -3,7 +3,10 @@
 package defaults
 
 // ControlAPISocket is the default path where clients can contact the swarmd control API.
-var ControlAPISocket = `npipe:////./pipe/docker_engine`
+var ControlAPISocket = "//./pipe/swarmd"
+
+// EngineAddr is Docker default named pipe on Windows
+var EngineAddr = "npipe:////./pipe/docker_engine" 
 
 // StateDir is the default path to the swarmd state directory
 var StateDir = `C:\ProgramData\swarmd`
