aa2aefb5c2b10ae5ee9844e7c892ec7977f9ce6a
Olli Janatuinen <olli.janatuinen@gmail.com>
2018-07-07 23:59:17 +0300

    J

--- cmd/swarmd/defaults/defaults_unix.go
+++ cmd/swarmd/defaults/defaults_unix.go
@@ -3,7 +3,7 @@
 package defaults
 
 // ControlAPISocket is the default path where clients can contact the swarmd control API.
-var ControlAPISocket = "/var/run/swarmd.sock"
+var ControlAPISocket = "unix:///var/run/docker.sock""
 
 // StateDir is the default path to the swarmd state directory
 var StateDir = "/var/lib/swarmd"
