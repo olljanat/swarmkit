f4ce0c8e5f05cb90eade29abcbdc4c798688ff20
Olli Janatuinen <olli.janatuinen@gmail.com>
2018-07-08 00:03:02 +0300

    x

--- cmd/swarmd/defaults/defaults_windows.go
+++ cmd/swarmd/defaults/defaults_windows.go
@@ -3,7 +3,7 @@
 package defaults
 
 // ControlAPISocket is the default path where clients can contact the swarmd control API.
-var ControlAPISocket = "npipe:////./pipe/docker_engine"
+var ControlAPISocket = `npipe:////./pipe/docker_engine`
 
 // StateDir is the default path to the swarmd state directory
 var StateDir = `C:\ProgramData\swarmd`
