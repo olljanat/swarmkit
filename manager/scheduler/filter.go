319e265a6d41e04e908fad70112ec7e3ebc5ac52
Olli Janatuinen <olli.janatuinen@gmail.com>
2018-10-01 18:38:32 +0000

    Slot logic

--- manager/scheduler/filter.go
+++ manager/scheduler/filter.go
@@ -379,7 +379,7 @@ func (f *MaxReplicasFilter) SetTask(t *api.Task) bool {
 
 // Check returns true if there is free slots for task in a given node.
 func (f *MaxReplicasFilter) Check(n *NodeInfo) bool {
-	if uint64(n.ActiveTasksCountByService[f.t.ServiceID]) < f.t.Spec.Placement.MaxReplicas {
+	if uint64(n.ActiveTasksCountByService[f.t.ServiceID]) < f.t.Spec.Placement.MaxReplicas && f.t.Slot <= f.t.Spec.Placement.MaxReplicas {
 		return true
 	}
 
