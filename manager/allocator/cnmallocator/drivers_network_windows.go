26191d64ab0b33a299b4d121910c057af2ccb412
Olli Janatuinen <olli.janatuinen@sdworx.com>
2023-02-12 16:47:46 +0100

    Windows: Support local drivers internal, l2-bridge and nat
    Signed-off-by: Olli Janatuinen <olli.janatuinen@sdworx.com>

--- manager/allocator/cnmallocator/drivers_network_windows.go
+++ manager/allocator/cnmallocator/drivers_network_windows.go
@@ -3,12 +3,18 @@ package cnmallocator
 import (
 	"github.com/docker/docker/libnetwork/drivers/overlay/ovmanager"
 	"github.com/docker/docker/libnetwork/drivers/remote"
+	"github.com/docker/docker/libnetwork/drivers/windows/imanager"
+	"github.com/docker/docker/libnetwork/drivers/windows/l2brmanager"
+	"github.com/docker/docker/libnetwork/drivers/windows/natmanager"
 	"github.com/moby/swarmkit/v2/manager/allocator/networkallocator"
 )
 
 var initializers = []initializer{
 	{remote.Init, "remote"},
 	{ovmanager.Init, "overlay"},
+	{imanager.Init, "internal"},
+	{l2brmanager.Init, "l2bridge"},
+	{natmanager.Init, "nat"},
 }
 
 // PredefinedNetworks returns the list of predefined network structures
